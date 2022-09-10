2f334511c34075c02b4a3d3cce2b8205fd64b207
hungf1324 <hungf1324@gmail.com>
2022-09-10 10:10:55 +0700

    first_commit

---
+++ lib/models/student_model.dart
@@ -0,0 +1,13 @@
+class StudentModel {
+  final int id;
+  final String name;
+  final String address;
+  final String phone;
+
+  StudentModel({
+    required this.id,
+    required this.name,
+    required this.address,
+    required this.phone,
+  });
+}
